afd40700f9255772846ce76cbd56be70f00a44ff
CIFCI73 <160320679+CIFCI73@users.noreply.github.com>
2025-12-12 16:34:32 +0000

    app setup2

---
+++ app/src/main/java/com/kaktus/app/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.kaktus.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Colori principali di Kaktus
+val KaktusGreen = Color(0xFF4A635D)      // Verde Opaco (Primary)
+val KaktusBeige = Color(0xFFE8E4D9)      // Beige Opaco (Background)
+
+// Varianti per contrasto e testi
+val KaktusDarkGreen = Color(0xFF2E3D39)  // Per testi su sfondo beige
+val KaktusLightBeige = Color(0xFFF7F5F0) // Per sfondi secondari o card
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
